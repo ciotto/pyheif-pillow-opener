c3df7c13c175e5e492fdafd0fcffe91eb3883bf7
Alexander Karpinsky <homm86@gmail.com>
2021-10-17 23:43:03 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,55 +1,32 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from setuptools import find_packages, setup
-import os
-try:
-    # pip >=20
-    from pip._internal.network.session import PipSession
-    from pip._internal.req import parse_requirements
-except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
+from setuptools import setup
+import os.path
 
 __version__ = '0.1.0'
 
-github_url = 'https://github.com/ciotto'
-package_name = 'pyheif-pillow-opener'
+github_url = 'https://github.com/uploadcare'
+package_name = 'heif-image-plugin'
 package_path = os.path.abspath(os.path.dirname(__file__))
-long_description_file_path = os.path.join(package_path, 'README.md')
-long_description = ''
-try:
-    install_requirements = [str(ir.req) for ir in parse_requirements('requirements.txt', session=PipSession())]
-except AttributeError:
-    install_requirements = [str(ir.requirement) for ir in parse_requirements('requirements.txt', session=PipSession())]
-try:
-    with open(long_description_file_path) as f:
-        long_description = f.read()
-except IOError:
-    pass
+
+with open(os.path.join(package_path, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name=package_name,
-    packages=find_packages(exclude=['docs', 'tests*']),
-    include_package_data=True,
+    py_modules=['HeifImagePlugin'],
     version=__version__,
-    description='pyheif-pillow-opener is a simple HEIF/HEIC opener for Pillow base on pyhief library.',
+    description='Simple HEIF/HEIC plugin for Pillow base on pyhief library.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='Christian Bianciotto',
-    author_email='info@ci8.it',
-    url='%s/%s' % (github_url, package_name, ),
-    download_url='%s/%s/archive/v%s.tar.gz' % (github_url, package_name, __version__, ),
-    keywords=['heif', 'heic', 'Pillow'],
-    install_requires=install_requirements,
+    author='Alexander Karpinsky',
+    author_email='homm86@gmail.com',
+    url='%s/%s' % (github_url, package_name),
+    download_url='%s/%s/archive/v%s.tar.gz' % (github_url, package_name, __version__),
+    keywords=['heif', 'heic', 'Pillow', 'plugin', 'pyhief'],
+    install_requires=[
+        "pyheif>=0.5.1",
+    ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         "Operating System :: MacOS :: MacOS X",
@@ -62,5 +39,5 @@ setup(
         'Topic :: Multimedia :: Graphics',
     ],
     license='MIT',
-    test_suite='tests'
+    test_suite='tests',
 )
